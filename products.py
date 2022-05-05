81ad164e5338634066479ff831fb0906fce6263c
Juicyyy <juicykfc28@gmail.com>
2022-05-05 16:04:24 +0800

    加入簡潔註解

--- products.py
+++ products.py
@@ -1,6 +1,6 @@
 # while loop 不知道迴圈幾次
 # for loop 清楚知道迴圈幾次
-product = []#product為一節車廂
+product = [] #product為一節車廂
 while True:
 	name  = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -8,6 +8,6 @@ while True:
 	price = input('請輸入商品價格: ')
 	p = [] #p車廂裡的座位
 	p.append(name)
-	p.append(price)
-	product.append(p)
+	p.append(price) #9 10 11行可簡寫成 p = [name , price]
+	product.append(p) #更簡潔版:product.append([name , price])
 print(product)
